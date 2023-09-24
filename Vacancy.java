fecc58fb97fb386f0aa67659981093ec63ceb6b3
Karina <usmkarin@gmail.com>
2023-09-24 13:45:54 +0700

    done

---
+++ Vacancy.java
@@ -0,0 +1,24 @@
+package observer;
+
+public class Vacancy {
+    
+    private String name;
+    private int salary;
+
+    public Vacancy(String name, int salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
