f8f926a3f6533047818ea4fc0b269164874b28ac
Karina <usmkarin@gmail.com>
2023-09-24 13:45:40 +0700

    done

---
+++ Student.java
@@ -0,0 +1,26 @@
+package observer;
+
+public class Student implements Observer{
+    
+    private String name;
+    private String jobTitle;
+    private int salary = 2000;
+
+    public Student(String name, String jobTitle) {
+        this.name = name;
+        this.jobTitle = jobTitle;
+    }
+
+
+    @Override
+    public boolean receiveOffer(String companyName, String jobTitle, int salary) {
+        if (this.salary <= salary && this.jobTitle.equals(jobTitle)) {
+            System.out.printf("Студент: %s: Мне нужна эта работа! (Компания: %s; Вакансия %s; Заработная плата: %d)\n", name, companyName, jobTitle, salary);
+        return true;
+        }    
+        else {
+            System.out.printf("Студент: %s: Я найду работу получше! (Компания: %s; Вакансия %s; Заработная плата: %d)\n", name, companyName, jobTitle, salary);
+        return false;
+        } 
+    }
+}
