07a16f992a657e655cebad11f7d6b48f5694eee8
Karina <usmkarin@gmail.com>
2023-09-24 13:44:38 +0700

    done

---
+++ JobAgency.java
@@ -0,0 +1,30 @@
+package observer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JobAgency implements Publisher{
+    
+    private List<Observer> observers = new ArrayList<>();
+
+    @Override
+    public void registerObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    @Override
+    public void removeObserver(Observer observer) {
+        observers.remove(observer);
+    }
+
+    @Override
+    public Observer sendOffer(String companyName, String jobTitle, int salary) {
+        for (Observer observer : observers) {
+            if(observer.receiveOffer(companyName, jobTitle, salary)){
+                System.out.println("Работник согласен на должность!");
+                return observer;
+            }
+        }
+        return null;
+    }
+}
